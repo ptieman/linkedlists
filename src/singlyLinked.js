b2f9a21d5b66aaee36c834dd9959a1ab48de4ff5
Presley Tieman <presley.tieman@gettyimages.com>
2023-04-24 10:53:33 -0400

    find middle of linkedlist

--- src/singlyLinked.js
+++ src/singlyLinked.js
@@ -79,6 +79,19 @@ class singlyLinkedList {
         }
     }
 
+    // find middle of linkedlist
+
+    findMiddle(){
+        let slow = this.head;
+        let fast = this.head;
+
+        while (fast && fast.next){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow.data;
+    }
+
     // search for node with  given data
     
     findNode(data) {
